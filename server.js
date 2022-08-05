3324a728e31c97a2b6d6e8ba6f4241866f8c8da5
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-05 23:23:06 +0700

    lab1.3

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	console.log(req.method, req.url, req.headers);
+	res.setHeader("Content-type", "text/html");
+	res.write("<html>");
+	res.write("<head><title>Hello,Here is My first Response page</title><head/>");
+	res.write("<body><h1>Hello from my NodeJs Server!!!</h1></body>");
+	res.write("</html>");
+	res.end();
 });
 
 server.listen(3000);
