91e4f5c7abeb7b0616f7ff63278730218ccc0ed2
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-05 23:50:43 +0700

    lab1.4

--- server.js
+++ server.js
@@ -2,7 +2,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req.method, req.url, req.headers);
+	const url = req.url;
+	const method = req.method;
+
+	if (url === "/") {
+		res.setHeader("Content-type", "text/html");
+		res.write("<html>");
+		res.write("<head><title>Login</title><head/>");
+		res.write(
+			"<body><form form action = '/message' method = 'POST' ><input type='text' name = 'mess'/><button type = 'submit'>send</button></form ></body > "
+		);
+		res.write("</html>");
+		return res.end();
+    }
+    if ((url = "/message")) {
+		}
+
 	res.setHeader("Content-type", "text/html");
 	res.write("<html>");
 	res.write("<head><title>Hello,Here is My first Response page</title><head/>");
