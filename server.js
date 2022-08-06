a14f6bb0aceacd70c473a987938b7f348c04e1f8
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-06 21:55:52 +0700

    lab2.2

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log("in the first middle ware!");
+	next();
+});
+
+app.use((res, req, next) => {
+	console.log("in the sencond middle ware!!!");
+});
+
 const server = http.createServer(app);
 server.listen(3000);
