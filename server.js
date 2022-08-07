14c32ec6a798e425fa09e05f3ee8b11315cb53db
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-07 11:58:36 +0700

    lab2.5

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 /** @format */
 
+const { urlencoded } = require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const server = express();
 
-server.use("/", (req, res, next) => {
-	console.log("this always runs");
-	next();
-});
+server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(bodyParser(undefined, { extends: false }));
 
 server.use("/add-product", (req, res, next) => {
 	console.log("in the first middleware");
-	res.send("<h1>The Add product page</h1>");
+	res.send(
+		"<form action='/product' method='POST'><input type ='text' name='title' placeHolder='product name'/><button type = 'submit'>add product</button></form>"
+	);
+});
+
+server.use("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
 });
 
 server.use("/", (req, res, next) => {
