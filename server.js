8e941eb0c20c4c9a7979aa8c498a9ce5b210c916
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-07 17:25:50 +0700

    lab2.14

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const express = require("express");
 
+const rootDir = require("./util/path");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/shop");
 
 const server = express();
-server.use(bodyParser.urlencoded({ extended: true }));
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static(path.join(rootDir, "public")));
 server.use("/admin", adminRouter);
 server.use(userRouter);
-
 server.use("/", (req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, "views", "page-not-found.html"));
+	res.status(404).sendFile(path.join(rootDir, "views", "page-not-found.html"));
 });
 
 server.listen(3000);
