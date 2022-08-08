edb766f156773fe2ce928de63925df474e8b7c28
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-08 21:03:51 +0700

    lab3.4

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.use(express.static(path.join(rootDir, "public")));
 server.use("/admin", admin.routes);
 server.use(userRouter);
 server.use("/", (req, res, next) => {
-	res.status(404).sendFile(path.join(rootDir, "views", "page-not-found.html"));
+	res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 server.listen(3000);
