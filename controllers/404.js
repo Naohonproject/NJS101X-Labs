40052bfe3baeaf1b9b9a61e2800a29b7290f586c
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-09 23:46:59 +0700

    replace with files supplied by MOOC

--- controllers/404.js
+++
@@ -1,4 +0,0 @@
-/** @format */
-exports.getNotFound = (req, res, next) => {
-	res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
-};
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
-/** @format */
-const express = require("express");
+const path = require('path');
 
-const productController = require("../controllers/product");
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get("/add-product", productController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post("/add-product", productController.postProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
