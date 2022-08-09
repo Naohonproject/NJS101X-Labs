6d9433bba5d7dec6fc6b62c9bef7ba955a746615
Le Tuan Bao <Letuanbao27121996@gmail.com>
2022-08-09 22:10:35 +0700

    lab4.4

--- model/product.js
+++ model/product.js
@@ -1,15 +1,30 @@
 /** @format */
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 class Product {
 	constructor(title) {
 		this.title = title;
 	}
 	saveProduct() {
-		products.push(this);
+		const p = path.join(path.dirname(require.main.filename), "data", "products.json");
+		fs.readFile(p, (err, content) => {
+			let products = [];
+			if (!err) {
+				products = JSON.parse(content);
+			}
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
 	}
 	static getAllProduct() {
-		return products;
+		const p = path.join(path.dirname(require.main.filename), "data", "products.json");
+		fs.readFile(p, (err, content) => {
+			if (err) return [];
+			return JSON.parse(content);
+		});
 	}
 }
 
