15f02588507040b9a745ee904671e2914ee54c2b
unknown <Letuanbao@mbg8.onmicrosoft.com>
2022-08-13 23:28:07 +0700

    lab5.9

--- models/product.js
+++ models/product.js
@@ -1,31 +1,57 @@
-/** @format */
-const db = require("../util/database");
+// /** @format */
+// const db = require("../util/database");
 
-const Cart = require("./cart");
+// const Cart = require("./cart");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
 
-  async save() {
-    return db.execute(
-      "INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)",
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
+//   async save() {
+//     return db.execute(
+//       "INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)",
+//       [this.title, this.price, this.description, this.imageUrl]
+//     );
+//   }
 
-  static async deleteById(id) {}
+//   static async deleteById(id) {}
 
-  static async fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
+//   static async fetchAll() {
+//     return db.execute("SELECT * FROM products");
+//   }
 
-  static async findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ? ", [id]);
-  }
-};
+//   static async findById(id) {
+//     return db.execute("SELECT * FROM products WHERE products.id = ? ", [id]);
+//   }
+// };
+
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: Sequelize.STRING,
+  allowNull: false,
+});
+
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,15 +1,4 @@
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-complete",
-//   password: "27121996",
-// });
-
-// module.exports = pool.promise();
-
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("node-complete", "root", "27121996", {
   dialect: "mysql",
