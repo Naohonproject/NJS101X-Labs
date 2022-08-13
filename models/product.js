5c2b74477a2377834e7a1d97e95f3063c14e7be8
unknown <Letuanbao@mbg8.onmicrosoft.com>
2022-08-13 17:29:33 +0700

    lab5.8

--- models/product.js
+++ models/product.js
@@ -25,7 +25,7 @@ module.exports = class Product {
     return db.execute("SELECT * FROM products");
   }
 
-  static findById(id) {
+  static async findById(id) {
     return db.execute("SELECT * FROM products WHERE products.id = ? ", [id]);
   }
 };
