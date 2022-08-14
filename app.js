fc7c77bfbb211ff1ea877fed45708178cbeb6cc4
unknown <Letuanbao@mbg8.onmicrosoft.com>
2022-08-14 16:28:21 +0700

    lab5.22

--- app.js
+++ app.js
@@ -40,8 +40,10 @@ Product.belongsTo(User, {
   constraints: true,
   onDelete: "CASCADE",
 });
+User.hasMany(Product);
 
 User.hasOne(Cart);
+Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
