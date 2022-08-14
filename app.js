d39e8c5bbe8cd11a897ea05bb469a9234fa40ed8
unknown <Letuanbao@mbg8.onmicrosoft.com>
2022-08-14 11:32:21 +0700

    lab5.18

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -23,8 +25,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE",
+});
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
 
