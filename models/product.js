68494878a33a1a18a6100bcb8c75f84e2c85b649
Yonko <Letuanbao@mbg8.onmicrosoft.com>
2022-08-15 22:20:42 +0700

    lab6.2

--- models/product.js
+++ models/product.js
@@ -1,38 +1,15 @@
-// /** @format */
-// const db = require("../util/database");
+const mongoConnect = require("../util/database");
 
-// const Cart = require("./cart");
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
 
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   async save() {
-//     return db.execute(
-//       "INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)",
-//       [this.title, this.price, this.description, this.imageUrl]
-//     );
-//   }
-
-//   static async deleteById(id) {}
-
-//   static async fetchAll() {
-//     return db.execute("SELECT * FROM products");
-//   }
-
-//   static async findById(id) {
-//     return db.execute("SELECT * FROM products WHERE products.id = ? ", [id]);
-//   }
-// };
-
-const Sequelize = require("sequelize");
-
-const sequelize = require("../util/database");
+  save() {}
+}
 
 const Product = sequelize.define("product", {
   id: {
