79df8bece7a00f55c63c42636879db150fee3717
Yonko <Letuanbao@mbg8.onmicrosoft.com>
2022-08-15 23:33:34 +0700

    lab6.3

--- util/database.js
+++ util/database.js
@@ -1,15 +1,26 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
     "mongodb+srv://letuanbao:SByQsXUanGc1VnuZ@cluster0.4ewgxhk.mongodb.net/?retryWrites=true&w=majority"
   )
     .then((client) => {
-      console.log("connected");
-      callback(client);
+      _db = client.db("NJS101X");
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
