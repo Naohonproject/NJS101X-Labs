46f03bfa041e4e6750cab6c7d32fe6f26bc91510
Yonko <Letuanbao@mbg8.onmicrosoft.com>
2022-08-15 22:08:42 +0700

    lab6.1

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const app = express();
 
 const errorController = require("./controllers/error");
-
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/oder-item");
-
-const app = express();
+const mongoConnect = require("./util/database");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -25,56 +17,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((error) => console.log(error));
+  // User.findById(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((error) => console.log(error));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: "CASCADE",
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
 });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-sequelize
-  .sync()
-  // .sync({ force: true })
-  .then((result) => {
-    return User.findById(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: "letuanbao",
-        email: "letuanbao27121996@gmail.com",
-      });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((error) => console.log(error));
