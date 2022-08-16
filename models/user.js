da85ac31741a9684fba15939e2d10fd3358a824f
Yonko <Letuanbao@mbg8.onmicrosoft.com>
2022-08-17 05:14:14 +0700

    lab6.16

--- models/user.js
+++ models/user.js
@@ -2,14 +2,31 @@ const { getDb } = require("../util/database");
 const mongodb = require("mongodb");
 
 class User {
-  constructor(userName, userEmail) {
+  constructor(userName, userEmail, cart, id) {
     this.name = userName;
     this.email = userEmail;
+    this.cart = cart;
+    this_id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex((item) => {
+      return item._id === product._id;
+    });
+
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      {
+        $set: { cart: updatedCart },
+      }
+    );
   }
 
   static findById(userId) {
