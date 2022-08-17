8623f886375a1bd5c7728868e7a26a9295c19554
Yonko <Letuanbao@mbg8.onmicrosoft.com>
2022-08-17 20:56:30 +0700

    lab6.19

--- models/user.js
+++ models/user.js
@@ -54,6 +54,29 @@ class User {
     );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((item) => {
+      return item.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find((item) => {
+              return item.productId.toString() === product._id.toString();
+            }).quantity,
+          };
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
