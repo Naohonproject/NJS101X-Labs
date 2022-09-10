51417fd2768836fe00f2d6c15dd3b0f338f752f3
LE TUAN BAO <LETUANBAO@mbg8.onmicrosoft.com>
2022-09-10 15:08:39 +0700

    add reviews ,comments

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ const server = express();
 // if we use to register middle ware, that include the routers , we need to write the the most specific router first, then
 // write the common router in the bottom OR we use server.get or server.post to point absolute router for incoming request
 server.use(bodyParser.urlencoded({ extended: true }));
+// declare the the folder that serve as public, this will always be served
 server.use(express.static(path.join(rootDir, "public")));
+// router
 server.use("/admin", adminRouter);
 server.use(userRouter);
 server.use("/", (req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, "views", "page-not-found.html"));
 });
 
+// event-loop to listen the incoming request event
 server.listen(3000);
